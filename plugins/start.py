c732bffdcf50ac164d2e46587afc73efd9860968
Hritu-king <87654408+Hritu-king@users.noreply.github.com>
2023-06-01 12:02:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,11 +6,11 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Updates Channel", url="https://t.me/SDBOTs_inifinity")
+        [InlineKeyboardButton("Updates Channel", url="https://t.me/Private_Bots")
       ],
       [ 
         InlineKeyboardButton(
-            "Support Group", url="https://t.me/SDBOTz")]
+            "Developer", url="https://t.me/Prime_Hritu")]
     ])
     welcomed = f"<b> Hey {message.from_user.first_name} , \n\nI'm YouTube DL Bot. I can download video or audio f\n rom Youtube. \n\nMade by @SDBOTs_inifinity 🇱🇰/help for More info </b>"
     await message.reply_text(welcomed, reply_markup=joinButton)
